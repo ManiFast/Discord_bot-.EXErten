e9090ab3d19cb9dbc8b607d59e7cbfbc9844a4f2
ManiFast <62830326+Stas-inside@users.noreply.github.com>
2021-08-24 22:08:22 -0700

    Add files via upload

---
+++ bot.js
@@ -0,0 +1,26 @@
+const { Client, Intents } = require('discord.js');
+const { token } = require('./config.json');
+
+const client = new Client({ intents: [Intents.FLAGS.GUILDS] });
+
+client.once('ready', () => {
+	console.log('Ready!');
+});
+
+client.on('interactionCreate', async interaction => {
+	if (!interaction.isCommand()) return;
+
+	const { commandName } = interaction;
+
+	if (commandName === 'ping') {
+		await interaction.reply('Pong!');
+	} else if (commandName === 'beep') {
+		await interaction.reply('Boop!');
+	} else if (commandName === 'server') {
+		await interaction.reply(`Server name: ${interaction.guild.name}\nTotal members: ${interaction.guild.memberCount}`);
+	} else if (commandName === 'user-info') {
+		await interaction.reply(`Your username: ${interaction.user.username}\nYour ID: ${interaction.user.id}`);
+	}
+});
+
+client.login(token);
